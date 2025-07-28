1a7dfca11412ce779a6263c90809614aa03eabfb
emmanuel <classicemmaeasy@gmail.com>
2025-07-28 22:57:38 +0100

    all added

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add', views.add, name='add_task'),
+    path('mark_as_done/<int:pk>', views.mark_as_done, name='mark_as_done'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from . models import Task
 from django.http import HttpResponse
 from .models import Task
@@ -9,13 +9,26 @@ def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
     print(tasks)
 
-    # context = {
-    #     'my_task': tasks
-    # } 
-    return render(request, 'home.html', {'all_task': tasks})
+    completed_tasks = Task.objects.filter(is_completed=True)
+    print(completed_tasks)
+
+    context = {
+        'all_task': tasks,
+        'completed_tasks': completed_tasks
+    } 
+    return render(request, 'home.html', context)
 
 
 def add(request):
     task = request.POST['task']
     Task.objects.create(task=task, is_completed=False)
     return redirect('/')
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('/')
+
+
